f50fb06ce9b64fdbd5b9fc26f89149e58e3a8acb
Alex Riss <00alexx@riss.at>
2022-03-16 22:15:21 +0100

    Load and extract data

--- src/SpmGrids.jl
+++ src/SpmGrids.jl
@@ -1,5 +1,223 @@
-module SpmGrids2
+module SpmGrids
 
-greet() = print("Hello World!")
+# using DataFrames
+using DataStructures: OrderedDict
+using Dates
+using TOML
+
+export load_grid, get_channel, get_parameter
+
+const VERSION = VersionNumber(TOML.parsefile(joinpath(@__DIR__, "../Project.toml"))["version"])
+
+mutable struct SpmGrid
+    filename::String
+    header::OrderedDict{String,String}
+    channel_names::Vector{String}
+    channel_units::Vector{String}
+
+    fixed_parameters::Vector{String}
+    experiment_parameters::Vector{String}
+    experiment_parameters_units::Vector{String}
+    points::Int
+
+    data::Array{Float32}
+
+    sweep_signal::String
+    sweep_signal_unit::String
+
+    size::Vector{Float64}
+    size_unit::String
+    center::Vector{Float64}
+    angle::Float64
+    pixelsize::Vector{Int}
+
+    bias::Union{Missing,Float64}
+    z_feedback::Union{Missing,Bool}
+    
+    start_time::DateTime
+    end_time::DateTime
+end
+SpmGrid(filename::String) = SpmGrid(
+        filename, OrderedDict(), String[], String[],
+        String[], String[], String[], 0,
+        Float32[],
+        "", "",
+        Float64[], "", Float64[], 0., Int[],
+        missing, missing,
+        DateTime(-1), DateTime(-1)
+    )
+
+
+"""
+    load_grid(filename::AbstractString; header_only::Bool=false)
+
+Loads a grid from a binary file. If `header_only` is `true`, then only the header is loaded.
+"""
+function load_grid(filename::AbstractString; header_only::Bool=false)::SpmGrid
+
+    ext = rsplit(filename, "."; limit=2)[end]
+    if ext != "3ds"
+        throw(ArgumentError("Unknown file type \"$ext\""))
+    end
+
+    grid = SpmGrid(filename)
+    num_parameters = 0
+
+    open(filename) do f
+        for line in eachline(f)
+            if line == ":HEADER_END:"  # end of header
+                break
+            elseif occursin("=", line)
+                key, value = split(line, "="; limit=2)
+                value = strip(value, '"')  # often the values are in quotes
+                grid.header[key] = value
+            end
+        end
+
+        if haskey(grid.header, "Grid dim")
+            grid.pixelsize = parse.(Int, split(grid.header["Grid dim"], " x "))
+        else
+            @warn "Grid dimensions are not specified in the file."
+        end
+
+        if haskey(grid.header, "Grid settings")
+            grid_settings = parse.(Float64, split(grid.header["Grid settings"], ";"))
+            grid.center = [grid_settings[1], grid_settings[2]]
+            grid.size = [grid_settings[3], grid_settings[4]]
+            grid.size_unit = "nm"
+            grid.angle = grid_settings[5]
+        else
+            @warn "Grid settings are not specified in the file."
+        end
+
+        grid.fixed_parameters = split(grid.header["Fixed parameters"], ";")
+        experiment_parameters_names_units = rsplit.(split(grid.header["Experiment parameters"], ";"), limit=2)
+        grid.experiment_parameters = first.(experiment_parameters_names_units)
+        grid.experiment_parameters_units = strip.(last.(experiment_parameters_names_units), (['(', ')'], ))
+
+        channels= split(grid.header["Channels"], ";")
+        channel_names_units = rsplit.(channels, limit=2)
+        grid.channel_names = first.(channel_names_units)
+        grid.channel_units = strip.(last.(channel_names_units), (['(', ')'], ))
+
+        sweep_signal_name_unit = rsplit(grid.header["Sweep Signal"], limit=2)
+        grid.sweep_signal = sweep_signal_name_unit[1]
+        grid.sweep_signal_unit = sweep_signal_name_unit[2]
+
+        num_parameters = parse(Int, grid.header["# Parameters (4 byte)"])
+
+        grid.points = parse(Int, grid.header["Points"])
+
+        grid.start_time = DateTime(grid.header["Start time"], dateformat"d.m.Y H:M:S")
+        grid.end_time = DateTime(grid.header["End time"], dateformat"d.m.Y H:M:S")
+    
+        if haskey(grid.header, "Z-Controller>Controller status")
+            grid.z_feedback = (grid.header["Z-Controller>Controller status"] == "ON") ? true : false
+        end
+
+        if haskey(grid.header, "Bias>Bias (V)")
+            grid.bias = parse(Float64, grid.header["Bias>Bias (V)"])
+        end
+
+        # read binary data
+        if !header_only
+            read_binary_data!(grid, f, num_parameters)
+        end
+    end
+
+
+    return grid
+end
+
+
+"""
+    read_binary_data!(grid::SpmGrid, f::IOStream, num_parameters::Int)
+
+Reads binary data from 3ds file. The file pointer should be set to the start of the binary data.
+"""
+function read_binary_data!(grid::SpmGrid, f::IOStream, num_parameters::Int)::Nothing
+    @assert num_parameters == length(grid.fixed_parameters) + length(grid.experiment_parameters)
+
+    data = Array{Float32}(undef, num_parameters + grid.points * length(grid.channel_names), grid.pixelsize...)
+
+    read!(f, data)
+    grid.data = ntoh.(data)  # big-endian to host endian
+
+    return nothing
+end
+
+
+"""
+    get_channel_index(grid::SpmGrid, name::AbstractString)::UnitRange{Int}
+
+For the channel data specified by `name`, the index range is returned. This can directly be used to index the `data` field of the `SpmGrid`.
+"""
+function get_channel_index(grid::SpmGrid, name::AbstractString)::UnitRange{Int}
+    i_channel = findfirst(isequal(name), grid.channel_names)
+    if i_channel === nothing
+        throw(ArgumentError("""Channel name "$(name)" not found in the SpmGrid. Available channel names are $(join(grid.channel_names, ", "))."""))
+    end
+
+    n_parameters = length(grid.fixed_parameters) + length(grid.experiment_parameters)
+
+    start = n_parameters + (i_channel - 1) * grid.points + 1
+    stop = start + grid.points - 1 
+
+    return start:stop
+end
+
+
+"""
+    get_channel(grid::SpmGrid, name::AbstractString, point_x::Int, point_y::Int)::Vector{Float32}
+
+Returns the data for the channel `name` at the point(s) specified by `index_x`, `index_y`
+The channel data can be indexed by `index_channel`.
+"""
+function get_channel(grid::SpmGrid, name::AbstractString, index_x::Any, index_y::Any, index_channel::Any=nothing)::Array{Float32}
+    idx = get_channel_index(grid, name)
+    if index_channel !== nothing
+        idx = idx[index_channel]
+    end
+    return grid.data[idx, index_x, index_y]
+end
+
+
+"""
+    get_channel(grid::SpmGrid, name::AbstractString, point_x::Int, point_y::Int)::Vector{Float32}
+
+Returns the data for the channel `name` at the point(s) specified by `index_x`, `index_y`.
+"""
+function get_channel(grid::SpmGrid, name::AbstractString, index_x::Any, index_y::Any)::Array{Float32}
+    idx = get_channel_index(grid, name)
+    return grid.data[idx, index_x, index_y]
+end
+
+
+"""
+    get_parameter_index(grid::SpmGrid, name::AbstractString)::Int
+
+Returns the index for the parameter specified by `name`. This can directly be used to index the `data` field of the `SpmGrid`.
+"""
+function get_parameter_index(grid::SpmGrid, name::AbstractString)::Int
+    par_names = vcat(grid.fixed_parameters, grid.experiment_parameters)
+    i_par = findfirst(isequal(name), par_names)
+    if i_par === nothing
+        throw(ArgumentError("""Parameter name "$(name)" not found in the SpmGrid. Available parameter names are $(join(par_names, ", "))."""))
+    end
+
+    return i_par
+end
+
+
+"""
+    get_parameter(grid::SpmGrid, name::AbstractString, index_x::Any, index_y::Any)::Union{Float32, Array{Float32}}
+
+Returns the value for parameter `name` at the point(s)specified by `index_x`, `index_y`.
+"""
+function get_parameter(grid::SpmGrid, name::AbstractString, index_x::Any, index_y::Any)::Union{Float32, Array{Float32}}
+    idx = get_parameter_index(grid, name)
+    return grid.data[idx, index_x, index_y]
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,40 @@
+using Dates
 using SpmGrids
 using Test
 
 @testset "loading" begin
+    grid = load_grid("Grid Spectroscopy002.3ds", header_only=true)
+
+    @test grid.pixelsize == [20,20]
+    @test grid.angle ≈ 0.
+    @test grid.center ≈ [1.106620E-7, 2.331660E-7]
+    @test grid.size ≈ [1.5e-8, 1.5e-8]
+
+    @test grid.points == 128
+
+    @test grid.bias ≈ 0.2
+    @test grid.z_feedback == false
+
+    @test grid.start_time == DateTime(2017,03,09,21,19,38)
+    @test grid.end_time == DateTime(2017,03,10,8,21,16)
+
+    @test length(grid.data) == 0  # 0 because `header_only` is true
+    @test length(grid.data_parameters) == 0  # 0 because `header_only` is true
+
+    @test grid.fixed_parameters == ["Sweep Start", "Sweep End"]
+    @test grid.experiment_parameters == ["X (m)", "Y (m)", "Z (m)", "Z offset (m)", "Settling time (s)", "Integration time (s)", "Z-Ctrl hold", "Final Z (m)", "Scan:Current (A)", "Scan:Applied Voltage measured (V)", "Scan:Bias (V)", "Scan:Z (m)", "Scan:Phase (deg)", "Scan:Amplitude (m)", "Scan:Frequency Shift (Hz)", "Scan:Excitation (V)"]
+    @test grid.channel_names == ["Current", "Applied Voltage measured", "Bias", "X", "Y", "Z", "Phase", "Amplitude", "Frequency Shift", "Excitation"]
+    @test grid.channel_units == ["A", "V", "V", "m", "m", "m", "deg", "m", "Hz", "V"]
+
+    @test grid.sweep_signal == "Bias"
+
+    @test grid.header["Oscillation Control>PLL-Setup Demod. Bandwidth Amp (Hz)"] == "80E+0"
+    @test grid.header["Z-Controller>Switch off delay (s)"] == "50E-3"
+    @test grid.header["Comment"] == "KPFM"
+    @test grid.header["Experiment"] == "Grid Spectroscopy"
+
+    grid = load_grid("Grid Spectroscopy002.3ds")
+
+    @test length(grid.data) == 20 * 20 * (18 + 10 * 128)  # pixels * (parameters + channels * points)
 end
 
