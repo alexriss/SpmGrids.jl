fcc1887cc8182844ab1a98544f7ea2bd06a40807
Alex Riss <00alexx@riss.at>
2022-03-17 10:01:42 +0100

    Load and extract data

--- docs/src/index.md
+++ docs/src/index.md
@@ -8,15 +8,23 @@ Documentation for [SpmGrids](https://github.com/alexriss/Spmgrids.jl).
 
 ## About
 
-A julia library to analyze scanning tunneling and atomic force spectroscopy data.
+A julia library to analyze scanning tuprobe microscopy grid data. Currently, only [Nanonis](https://www.specs-group.com/nanonis/products/) files are supported.
 
 ## Usage
 
 ```julia
-using Spmgrids
+using SpmGrids
 
-s = load_grid("Bias_spectrocopy.3ds")
+grid = load_grid("Bias_spectrocopy.3ds")
 
+c = get_channel(grid, "Current", 5, 5)  # get the current channel at point 5,5
+c = get_channel(grid, "Current", :, 5)  # get the current channel for fifth row
+c = get_channel(grid, "Current", :, 5, 20)  # get the 20th point of the current channel for fifth row
+c = get_channel(grid, "Current", :, 5:6, 1:50)  # get the first 50 points of the current channel for fifth and sixth rows, returns a 50x20x2 Array
+
+x = get_channel(grid, grid.sweep_signal, 5, 6)  # get the sweep signal at point 5,6
+
+p = get_parameter(grid, "Z offset", 3, 5)  # get the `Z offset` parameter at point 3,5
 ```
 
 A more detailed description can be found in the [Reference](@ref)
--- test/runtests.jl
+++ test/runtests.jl
@@ -19,10 +19,11 @@ using Test
     @test grid.end_time == DateTime(2017,03,10,8,21,16)
 
     @test length(grid.data) == 0  # 0 because `header_only` is true
-    @test length(grid.data_parameters) == 0  # 0 because `header_only` is true
 
     @test grid.fixed_parameters == ["Sweep Start", "Sweep End"]
-    @test grid.experiment_parameters == ["X (m)", "Y (m)", "Z (m)", "Z offset (m)", "Settling time (s)", "Integration time (s)", "Z-Ctrl hold", "Final Z (m)", "Scan:Current (A)", "Scan:Applied Voltage measured (V)", "Scan:Bias (V)", "Scan:Z (m)", "Scan:Phase (deg)", "Scan:Amplitude (m)", "Scan:Frequency Shift (Hz)", "Scan:Excitation (V)"]
+    @test grid.experiment_parameters == ["X", "Y", "Z", "Z offset", "Settling time", "Integration time", "Z-Ctrl", "Final Z", "Scan:Current", "Scan:Applied Voltage measured", "Scan:Bias", "Scan:Z", "Scan:Phase", "Scan:Amplitude", "Scan:Frequency Shift", "Scan:Excitation"]
+    @test grid.experiment_parameters_units == ["m", "m", "m", "m", "s", "s", "hold", "m", "A", "V", "V", "m", "deg", "m", "Hz", "V"]
+    @test grid.fixed_parameters == ["Sweep Start", "Sweep End"]
     @test grid.channel_names == ["Current", "Applied Voltage measured", "Bias", "X", "Y", "Z", "Phase", "Amplitude", "Frequency Shift", "Excitation"]
     @test grid.channel_units == ["A", "V", "V", "m", "m", "m", "deg", "m", "Hz", "V"]
 
@@ -36,5 +37,11 @@ using Test
     grid = load_grid("Grid Spectroscopy002.3ds")
 
     @test length(grid.data) == 20 * 20 * (18 + 10 * 128)  # pixels * (parameters + channels * points)
+    @test size(get_channel(grid, "Bias", 4:20, 5:5)) == (128,17,1)
+    @test size(get_channel(grid, "Frequency Shift", 4:20, 5:7, 20:23)) == (4,17,3)
+    @test get_channel(grid, "Bias", 20, 7, 20) ≈ 0.15511811f0
+    @test all(get_parameter(grid, "Z offset", 3, :) .≈ 0.0)
+    @test get_parameter(grid, "Z", 3, 5) ≈ -1.1132063f-8
+    @test get_channel(grid, "Current", 20, 7, 20:24) ≈ Float32[1.3028699f-10, 1.2868269f-10, 1.2712124f-10, 1.2609777f-10, 1.2497206f-10]
 end
 
